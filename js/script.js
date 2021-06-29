c4ba2701690d05bba3e9ebc2fa15444c98bf7464
Dainyleen Masangkay <dainyleen@gmail.com>
2021-06-29 21:22:26 +1200

    Hide Other Job Role field by default

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // Create a variable to "Name" input field
 const nameInput = document.getElementById('name')
-
 //Give name input field focus
 nameInput.focus()
+
+// Create variables to reference the "Job Role" and the "Other Job Role"
+const jobRole = document.getElementById('title')
+const otherJobRole = document.getElementById('other-job-role')
+
+//Hide "Other Job Role" by default
+otherJobRole.style.display = 'none'
+
+
